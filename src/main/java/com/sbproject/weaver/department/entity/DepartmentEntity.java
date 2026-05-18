8f9bec033e3d01b1885ec268314a06e96f1f80de
gimhan-yeong <hanyung0223@gmail.com>
2026-05-18 17:33:30 +0900

    feat: UUID 생성 전략을 v7로 변경

--- src/main/java/com/sbproject/weaver/department/entity/DepartmentEntity.java
+++ src/main/java/com/sbproject/weaver/department/entity/DepartmentEntity.java
@@ -1,10 +1,12 @@
 package com.sbproject.weaver.department.entity;
 
+import com.github.f4b6a3.uuid.UuidCreator;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import java.time.LocalDate;
 import java.util.UUID;
@@ -26,6 +28,12 @@ public class DepartmentEntity {
   @GeneratedValue(strategy = GenerationType.UUID)
   @Column(unique = true, nullable = false)
   private UUID id;
+  @PrePersist
+  void init() {
+    if (id == null) {
+      id = UuidCreator.getTimeOrdered(); // = UUID v7
+    }
+  }
 
   @Column(unique = true,nullable = false)
   private String name;
