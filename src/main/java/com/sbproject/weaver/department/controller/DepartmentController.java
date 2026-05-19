34d5a9700701cc2c8f9fec6749a461a9d0b0dc92
gimhan-yeong <hanyung0223@gmail.com>
2026-05-19 12:39:12 +0900

    refactor: DepartmentRepository 인터페이스 제거 및 어노테이션 추가

--- src/main/java/com/sbproject/weaver/department/controller/DepartmentController.java
+++ src/main/java/com/sbproject/weaver/department/controller/DepartmentController.java
@@ -4,6 +4,7 @@ import com.sbproject.weaver.department.dto.CreateRequest;
 import com.sbproject.weaver.department.dto.DepartmentDto;
 import com.sbproject.weaver.department.service.DepartmentService;
 import java.util.UUID;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,13 +16,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/departments")
 public class DepartmentController {
 
   private final DepartmentService departmentService;
-  public DepartmentController(DepartmentService departmentService) {
-    this.departmentService = departmentService;
-  }
 
   @PostMapping
   public ResponseEntity<DepartmentDto> create(@RequestBody CreateRequest request) {
